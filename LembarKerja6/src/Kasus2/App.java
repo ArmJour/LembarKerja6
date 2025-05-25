a04e7173901c461ddd45b176066a913787bd76e0
Raditya Arsyad <raditarsyad07@gmail.com>
2025-05-25 14:07:04 +0700

    Simplified the App code

--- LembarKerja6/src/Kasus2/App.java
+++ LembarKerja6/src/Kasus2/App.java
@@ -4,15 +4,10 @@ import javax.swing.SwingUtilities;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Create instances of Inventory and StockManager
-        Inventory inventory = new Inventory(); //
-        StockManager stockManager = new StockManager(inventory); //
-
-        // Run the GUI on the Event Dispatch Thread
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                // Use the new CombinedGUIManager
-                new GUIManager(stockManager);
+                // Use GUIManager 
+                new GUIManager(new StockManager(new Inventory()));
             }
         });
     } 
